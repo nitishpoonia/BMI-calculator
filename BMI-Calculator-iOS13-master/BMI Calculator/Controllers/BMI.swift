64e6532d671f7fabe973cffbeebce7bd9f171c10
Nitish Poonia <56960128+nitishpoonia@users.noreply.github.com>
2020-12-19 11:05:16 +0530

    Add files via upload

---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Nitish Poonia on 14/12/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Nitish Poonia on 13/12/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+  
+
+}
